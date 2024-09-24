c1333e1eb1fe436658464edf403af5702e635fcb
jahanRazh <jahanrazh@gmail.com>
2024-09-24 12:46:21 +0530

    Refactor circle skill section in main.js and remove unused code in style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,24 +115,3 @@ sections.forEach(current =>{
 }
 
 window.addEventListener('scroll', scrollActive)
-
-// circle skill /////////////
-
-const circles = document.querySelectorAll(".circle");
-circles.forEach((elem) => {
-  var dots = elem.getAttribute("data-dots");
-  var marked = elem.getAttribute("data-percent");
-  var percent = Math.floor((dots * marked) / 100);
-  var points = "";
-  var rotate = 360 / dots;
-
-  for (let i = 0; i < dots; i++) {
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-  }
-  elem.innerHTML = points;
-
-  const pointsMarked = elem.querySelectorAll(".points");
-  for (let i = 0; i < percent; i++) {
-    pointsMarked[i].classList.add("active");
-  }
-});
